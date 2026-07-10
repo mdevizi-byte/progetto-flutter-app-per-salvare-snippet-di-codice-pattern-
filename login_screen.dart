77990316589106120af0c5d3b0be7dedf038e425
Mattia De Vizi <mdevizi@studenti.laziodigital.it>
2026-07-10 16:56:32 +0200

    Rimuovi l'override non necessario dal metodo build nella schermata di accesso

--- login_screen.dart
+++ login_screen.dart
@@ -4,7 +4,6 @@ import 'register_screen.dart';
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
